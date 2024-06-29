d1557ced9fee0225aed698c4abe604654aeaee00
pedroyoung95 <jty9531@gmail.com>
2024-06-29 17:13:04 +0900

    웹소설 썸네일 및 제목 추출

--- bs4/naver_webnovel_title.py
+++ bs4/naver_webnovel_title.py
@@ -1,16 +1,37 @@
+import errno
+
 from bs4 import BeautifulSoup as bs
 from pprint import pprint
-import requests
+from urllib.request import urlretrieve
+import requests, re, os
 
+#저장 폴더 생성
+try:
+    if not (os.path.isdir('image')):
+        os.makedirs(os.path.join('image'))
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        print("폴더 생성 실패!")
+        exit()
+
+#웹 페이지 읽고 소스코드 읽기
 html = requests.get('https://novel.naver.com/webnovel/weekday')
-
 soup = bs(html.text, 'html.parser')
 html.close()
 
-data1 = soup.find('div', {'class':'component_section'})
+#웹소설 통합랭킹 영역 추출
+data1_list = soup.findAll( 'ul', {'class':'ranking_list type1 NE=a:all'})
 
-data2 = data1.findAll('span', {'class':'title'})
+#통합랭킹 웹소설
+novel_list = []
+for data1 in data1_list:
+    #웹소설 썸네일 및 제목 영역 추출
+    novel_list.extend(data1.findAll('li'))
 
-title_list = [t.text for t in data2]
-
-pprint(title_list)
+#각각의 썸네일과 제목 추출
+for novel in novel_list:
+    img = novel.find('img')
+    title = img['alt']
+    img_src = img['src']
+    title = re.sub('[^0-9a-zA-zㄱ-힗]','', title)
+    urlretrieve(img_src, './image/'+title+'.jpg')
